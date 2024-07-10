61b229b454b47b1a68ef2e92d5562e054a5401a7
AnjaliBhamare19 <1002bhamarear@gmail.com>
2024-07-10 22:47:22 +0530

    MERN

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
+import React, { useState } from 'react'
+import CNavbar from './components/Navbar/Navbar'
 import { Routes,Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Footer from './components/Footer/Footer'
-
+import Login from './components/Login/Login'
 const App = () => {
+
+  const [showLogin,setShowLogin] = useState(false);
   return (
     <>
+    {showLogin?<Login /> : <></>}
     <div className='app'>
-      <Navbar/>
+      <CNavbar setShowLogin={setShowLogin}/>
       <Routes>
          <Route path='/' element= {<Home />}/>
          <Route path='/cart' element= {<Cart />}/>
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,30 +1,72 @@
-import React, { useState } from 'react'
-import './Navbar.css'
-import { assets } from '../../assets/assets'
-import { Link } from 'react-router-dom'
-const Navbar = () => {
+import React, { useState } from 'react';
+import './Navbar.css';
+import { assets } from '../../assets/assets';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
- const [menu,setMenu] = useState("menu");
+const CNavbar = () => {
+  const [menu, setMenu] = useState("menu");
 
   return (
-    <div className='navbar'>
-       <img src={assets.logo} alt='logo' className='logo'/>
-       <ul className='navbar-menu'>
-        <Link to= '/'onClick={() => setMenu("home")}className={menu === "home" ? "active" : ""}>Home</Link>
-        <a href="#explore-menu" onClick={() => setMenu("menu")}className={menu === "menu" ? "active" : ""}>Menu</a>
-        <a href="#" onClick={() => setMenu("about-us")}className={menu === "about-us" ? "active" : ""}>About Us</a>
-        <a href="#footer" onClick={() => setMenu("contact")}className={menu === "contact" ? "active" : ""}>Contact</a>
-       </ul>  
-       <div className="navbar-right">
-          <img src={assets.search_icon} alt=''/>
-          <div className="navbar-search-icon">
-            <img src={assets.basket_icon} alt=''/>
-            <div className="dot"></div>
+    <Navbar expand="lg" className="bg-body-tertiary pt-0 pb-2" bg="light" data-bs-theme="light">
+      <Container fluid>
+        <Navbar.Brand href="#">
+          <img
+            alt=""
+            src={assets.logo}
+            className="logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll" className="justify-content-center">
+          <Nav
+            className="mx-auto my-2 my-lg-0"
+            style={{ maxHeight: '100%' }}
+            navbarScroll
+          >
+            <Nav.Link
+              href="/"
+              onClick={() => setMenu("home")}
+              className={menu === "home" ? "active" : ""}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              href="#explore-menu"
+              onClick={() => setMenu("menu")}
+              className={menu === "menu" ? "active" : ""}
+            >
+              Menu
+            </Nav.Link>
+            <Nav.Link
+              href="#"
+              onClick={() => setMenu("about-us")}
+              className={menu === "about-us" ? "active" : ""}
+            >
+              About Us
+            </Nav.Link>
+            <Nav.Link
+              href="#footer"
+              onClick={() => setMenu("contact")}
+              className={menu === "contact" ? "active" : ""}
+            >
+              Contact
+            </Nav.Link>
+          </Nav>
+          <div className="navbar-right d-flex align-items-center">
+            <img src={assets.search_icon} alt="" className="me-2" />
+            <div className="navbar-search-icon position-relative me-2">
+              <img src={assets.basket_icon} alt="" />
+              <div className="dot position-absolute top-0 start-100 translate-middle"></div>
+            </div>
+            <Button variant="primary">Sign In</Button>
           </div>
-          <button >Sign In</button>
-       </div>
-    </div>
-  )
-}
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
-export default Navbar
+export default CNavbar;
