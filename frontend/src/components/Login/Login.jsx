91690f23ef3dcc2179a285a094606e817d6eaba2
AnjaliBhamare19 <1002bhamarear@gmail.com>
2024-07-12 23:24:38 +0530

    MERN

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -2,24 +2,40 @@ import React, { useState } from 'react'
 import './Login.css'
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-const Login = () => {
-  const [currState,setCurrState] = useState("Sign In")
+import CloseButton from 'react-bootstrap/CloseButton';
+const Login = ({ setShowLogin }) => {
+  const [currState, setCurrState] = useState("Login")
   return (
     <Form>
-      <h2>{currState}</h2>
-    <Form.Group className="mb-3" controlId="formBasicEmail">
-      <Form.Label>Email </Form.Label>
-      <Form.Control type="email" placeholder="Enter email" />
-    </Form.Group>
+      <div className='login-title'>
+        <h2>{currState}</h2>
+        <CloseButton onClick={() => setShowLogin(false)} />
+      </div>
 
-    <Form.Group className="mb-3" controlId="formBasicPassword">
-      <Form.Label>Password</Form.Label>
-      <Form.Control type="password" placeholder="Password" />
-    </Form.Group>
-    <Button variant="primary" type="submit">
-      Submit
-    </Button>
-  </Form>
+      <div className='login-input'>
+        {currState === "Login" ? <></> :
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Name </Form.Label>
+            <Form.Control type="text" placeholder="Enter Name" />
+          </Form.Group>
+        }
+        
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>E-mail </Form.Label>
+          <Form.Control type="email" placeholder="Enter Email" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Enter Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          {currState === "SignUp" ? "Create an Account" : "Login"}
+        </Button>
+        <Form.Check type={'checkbox'}
+          label="I agree to the terms and conditions." />
+      </div>
+    </Form>
   );
 }
 
